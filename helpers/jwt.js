acfcaf193c5ca8d6589fe67aa34d97dc8e693f37
AymanMoka <aymanmoka94@icloud.com>
2023-05-28 19:13:14 +0300

    bug fix

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,24 +5,25 @@ const authJwt = () => {
     secret: process.env.JWT_SECRET,
     algorithms: ["HS256"],
     // isRevoked: isRevoked,
-    getToken: (req) => {  
+    getToken: (req) => {
       if (
         req.headers.authorization &&
         req.headers.authorization.split(" ")[0] === "Bearer"
       ) {
         const token = req.headers.authorization.split(" ")[1]; //return token
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
           if (err) {
             return null;
           }
           req.user = decoded;
-        })
+        });
         return token;
       }
       return null;
     },
   }).unless({
     path: [
+      { url: "/api/v1/", methods: ["GET"] },
       { url: "/api/v1/login", methods: ["POST"] },
       { url: "/api/v1/register", methods: ["POST"] },
       //public uploads
